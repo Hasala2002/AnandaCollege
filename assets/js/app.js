a84ec96fcaab67dc30286ca2d29d8e874afff7fa
chamindujs <chamindujs@gmail.com>
2021-07-20 13:32:21 +0530

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,81 @@
+/*const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h2");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+ 
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
+
+  video.currentTime = delay;
+}, 33.3); */
+
+const inner = document.querySelector(".inner");
+const section = document.querySelector("section");
+
+window.onscroll = function() {
+  let value = window.pageYOffset / section.offsetTop + 1;
+  inner.style.transform = `scale(${value})`;
+};
+
+
+$(function(){
+	
+	var $window = $(window);		//Window object
+	
+	var scrollTime = 1.2;			//Scroll time
+	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+		
+	$window.on("mousewheel DOMMouseScroll", function(event){
+		
+		event.preventDefault();	
+										
+		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+			
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+				autoKill: true,
+				overwrite: 5							
+			});
+					
+	});
+	
+});
